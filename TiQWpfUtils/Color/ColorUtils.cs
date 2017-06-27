cd4f93e61d4403f98ce522c3701a57307577afc0
Max <grimfire@gmail.com>
2017-06-27 23:21:22 +0300

    init

---
+++ TiQWpfUtils/Color/ColorUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TiQWpfUtils
+{
+    public static class ColorUtils
+    {
+        public static Color GetColorFromLevel(int level)
+        {
+            Color clr;
+            switch (level)
+            {
+                case 0:
+                    clr = Color.FromRgb(255, 0, 0);
+                    break;
+                case 1:
+                    clr = Color.FromRgb(0, 0, 0);
+                    break;
+                case 2:
+                    clr = Color.FromRgb(0, 20, 93);
+                    break;
+                default:
+                    clr = Color.FromRgb(0, 0, 0);
+                    break;
+            }
+            return clr;
+        }
+    }
+}
