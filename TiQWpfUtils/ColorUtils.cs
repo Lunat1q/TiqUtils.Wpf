95836cc3b5cc727ebd7685ed0cbe4f9d16fe0699
Max <grimfire@gmail.com>
2017-07-06 20:23:21 +0300

    t

---
+++ TiQWpfUtils/ColorUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media;
+namespace TiQWpfUtils
+{
+    public static class ColorUtils
+    {
+        public static Color GetColorFromLevel(int level)
+        {
+            Color clr;
+            switch (level)
+            {
+                case 0:
+                    clr = Color.FromRgb(255, 0, 0);
+                    break;
+                case 1:
+                    clr = Color.FromRgb(0, 0, 0);
+                    break;
+                case 2:
+                    clr = Color.FromRgb(0, 20, 93);
+                    break;
+                default:
+                    clr = Color.FromRgb(0, 0, 0);
+                    break;
+            }
+            return clr;
+        }
+    }
+}
