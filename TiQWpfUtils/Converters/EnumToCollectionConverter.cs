d8f3867999f7f040721a8b6ecbb41259298d79a3
Max <grimfire@gmail.com>
2017-09-16 21:31:14 +0300

    converters

---
+++ TiQWpfUtils/Converters/EnumToCollectionConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using TiQWpfUtils.Helpers;
+
+namespace TiQWpfUtils.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(IEnumerable<ValueDescription>))]
+    public class EnumToCollectionConverter : MarkupExtension, IValueConverter
+    {
+        private static readonly ConcurrentDictionary<string, IEnumerable<ValueDescription>> DescriptorCache = new ConcurrentDictionary<string, IEnumerable<ValueDescription>>();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var skip = false;
+            if (parameter is bool)
+            {
+                skip = (bool) parameter;
+            }
+            return TryGetFromCache(value?.GetType(), skip);
+        }
+
+        private object TryGetFromCache(Type type, bool skip)
+        {
+            var key = GenerateKey(type, skip);
+            if (DescriptorCache.ContainsKey(key))
+            {
+                return DescriptorCache[key];
+            }
+            var desc = EnumHelper.GetAllValuesAndDescriptions(type, skip);
+            DescriptorCache.TryAdd(key, desc);
+            return desc;
+        }
+
+        private string GenerateKey(Type type, bool skip)
+        {
+            return type.FullName + (skip ? "!" : "#");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ TiQWpfUtils/Converters/GetLabelForEnum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+using TiQWpfUtils.Controls.Extensions.DataGrid;
+
+namespace TiQWpfUtils.Converters
+{
+    public sealed class GetLabelForEnum : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value as Enum;
+            if (val != null)
+            {
+                var bVal = val;
+                var type = bVal.GetType();
+                var memInfo = type.GetMember(bVal.ToString());
+                var attribute = memInfo[0].GetCustomAttribute(typeof(LabelNameAttributeBase), false) as LabelNameAttributeBase;
+                if (attribute != null)
+                {
+                    return attribute.GetProperText();
+                }
+            }
+            return value?.ToString() ?? "%empty_label%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
